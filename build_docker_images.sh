86a0fef7ed1405a86321812dbb470b84955da998
Paul O'Neil <redballoon36@gmail.com>
2014-10-28 21:06:53 -0400

    Rename the docker namespace for our images since '-' is not allowed

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -2,7 +2,7 @@
 
 VERSION="0.1"
 
-docker build -t="bdslss-pkss/hadoop:${VERSION}" Hadoop
-docker build -t="bdslss-pkss/resource-manager:${VERSION}" ResourceManager
-docker build -t="bdslss-pkss/name-node:${VERSION}" NameNode
-docker build -t="bdslss-pkss/worker:${VERSION}" Worker
+docker build -t="bdslss_pkss/hadoop:${VERSION}" Hadoop
+docker build -t="bdslss_pkss/resource_manager:${VERSION}" ResourceManager
+docker build -t="bdslss_pkss/name-node:${VERSION}" NameNode
+docker build -t="bdslss_pkss/worker:${VERSION}" Worker
