2019c5eb687b7c0128be5715346001a6a6dbcee7
Paul O'Neil <redballoon36@gmail.com>
2014-10-29 13:05:08 -0400

    Use underscore over - consistently

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -4,5 +4,5 @@ VERSION="0.1"
 
 docker build -t="bdslss_pkss/hadoop:${VERSION}" Hadoop
 docker build -t="bdslss_pkss/resource_manager:${VERSION}" ResourceManager
-docker build -t="bdslss_pkss/name-node:${VERSION}" NameNode
+docker build -t="bdslss_pkss/name_node:${VERSION}" NameNode
 docker build -t="bdslss_pkss/worker:${VERSION}" Worker
