acad9ea5670772d2242e69a1aff886c106fbe1ba
Paul O'Neil <redballoon36@gmail.com>
2014-10-29 20:07:01 -0400

    Start the HDFS Name Node with runit, i.e. automatically on startup

---
+++ docker/NameNode/start_namenode.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+# What the docs say:
+# /root/hadoop-2.5.1/sbin/hadoop-daemon.sh --config /root/hadoop-2.5.1/etc/hadoop/ --script hdfs start namenode
+/root/hadoop-2.5.1/bin/hdfs --config /root/hadoop-2.5.1/etc/hadoop/ namenode
