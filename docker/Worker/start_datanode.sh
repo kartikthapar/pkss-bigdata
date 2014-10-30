31c64fec7af00308c61ed0498094bed35b40576d
Paul O'Neil <redballoon36@gmail.com>
2014-10-29 21:38:37 -0400

    Use the HADOOP_PREFIX and HADOOP_CONF_DIR variables

--- docker/Worker/start_datanode.sh
+++ docker/Worker/start_datanode.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # What the docs say:
 # $HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
-/root/hadoop-2.5.1/bin/hdfs --config /root/hadoop-2.5.1/etc/hadoop/ datanode
+$HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR datanode
--- docker/Worker/start_nodemanager.sh
+++ docker/Worker/start_nodemanager.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # What the docs say:
 # $HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start nodemanager
-/root/hadoop-2.5.1/bin/yarn --config /root/hadoop-2.5.1/etc/hadoop/ nodemanager
+$HADOOP_PREFIX/bin/yarn --config $HADOOP_CONF_DIR nodemanager
