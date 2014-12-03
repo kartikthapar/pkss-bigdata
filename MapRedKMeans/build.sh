b41caa4e319fcbd4739a9bec373243fd52325d44
Paul O'Neil <redballoon36@gmail.com>
2014-12-03 23:04:04 +0000

    Build a jar we can pass to hadoop!

--- MapRedKMeans/build.sh
+++ MapRedKMeans/build.sh
@@ -3,3 +3,5 @@ javac \
 	-classpath .:/root/hadoop-2.5.1/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.5.1.jar:/root/hadoop-2.5.1/share/hadoop/common/hadoop-common-2.5.1.jar:/root/hadoop-2.5.1/share/hadoop/mapreduce/lib/hadoop-annotations-2.5.1.jar \
 	-Xlint:deprecation \
 	*.java
+
+jar cf PKSS.jar *.class
