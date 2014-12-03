27b4fcb730dcd4fd51b4b099ad8cbbceaf2ed5ad
Paul O'Neil <redballoon36@gmail.com>
2014-12-03 03:46:57 +0000

    Force javac to recompile every class every time

--- MapRedKMeans/build.sh
+++ MapRedKMeans/build.sh
@@ -2,4 +2,4 @@
 javac \
 	-classpath .:/root/hadoop-2.5.1/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.5.1.jar:/root/hadoop-2.5.1/share/hadoop/common/hadoop-common-2.5.1.jar:/root/hadoop-2.5.1/share/hadoop/mapreduce/lib/hadoop-annotations-2.5.1.jar \
 	-Xlint:deprecation \
-	KMeans.java
+	*.java
