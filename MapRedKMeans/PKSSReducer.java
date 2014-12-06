2c4aee45fb867915b5a95930c3e6b26c66b058b5
Paul O'Neil <redballoon36@gmail.com>
2014-12-06 00:42:33 -0500

    Write out cluster assignments

--- MapRedKMeans/PKSSReducer.java
+++ MapRedKMeans/PKSSReducer.java
@@ -1,4 +1,7 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -14,18 +17,28 @@ public class PKSSReducer extends Reducer<LongWritable, Text, LongWritable, Text>
     protected void setup(Context context)
     {
         Configuration conf = context.getConfiguration();
-        assignment_dir = new org.apache.hadoop.fs.Path(conf.get ("clusterInput"));
+        // FIXME make the string a symbolic constant
+        assignment_dir = new org.apache.hadoop.fs.Path(conf.get ("assignmentOutput"));
     }
 
     @Override
-    public void reduce(LongWritable key, Iterable<Text> Value, Context context) throws java.io.IOException, InterruptedException
+    public void reduce(LongWritable key, Iterable<Text> Value, Context context)
+        throws java.io.IOException, InterruptedException
     {
+        // These are the intermediate results of computing the new cluster center
         long counter = 0;
         VectorizedObject thisCluster = null;
+
+        // These are used for storing the cluster assignments
+        // TODO make the write optional depending on configuration
+        FileSystem fs = FileSystem.get(context.getConfiguration());
+        Path cluster_output = new Path(assignment_dir, key.toString());
+        FSDataOutputStream assign_strm = fs.create(cluster_output);
+
         for (Text curText : Value)
         {
             VectorizedObject curDataPoint = new VectorizedObject(curText.toString());
-            if (thisCluster == null)
+            if (thisCluster == null) // FIXME Paul doesn't really like doing this check on every iteration
             {
                 thisCluster = curDataPoint;
             }
@@ -35,7 +48,11 @@ public class PKSSReducer extends Reducer<LongWritable, Text, LongWritable, Text>
             }
 
             counter += 1;
+
+            assign_strm.writeChars(curDataPoint.getKey().toString());
+            assign_strm.writeChar('\n');
         }
+        assign_strm.close();
 
         thisCluster.getLocation().multiplyMyselfByHim(1.0 / counter);
 
