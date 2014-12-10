2adfe4f856175366d2e465aca34837e25dc7db19
Simar <skoder91@gmail.com>
2014-12-10 02:44:23 +0000

    mini-driver file

--- file_transport/ClusterOrganization.java
+++ file_transport/ClusterOrganization.java
@@ -12,8 +12,6 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FSDataOutputStream;
 
 public class ClusterOrganization {
 	private Map<Integer, String> keyValueStore;
--- file_transport/build.sh
+++ file_transport/build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 javac \
-	-classpath .:${HADOOP_PREFIX}/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.5.1.jar:${HADOOP_PREFIX}/share/hadoop/common/hadoop-common-2.5.1.jar:${HADOOP_PREFIX}/share/hadoop/mapreduce/lib/hadoop-annotations-2.5.1.jar \
+	-classpath .:${HADOOP_PREFIX}/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.5.1.jar:${HADOOP_PREFIX}/share/hadoop/common/hadoop-common-2.5.1.jar:${HADOOP_PREFIX}/share/hadoop/mapreduce/lib/hadoop-annotations-2.5.1.jar:${HADOOP_PREFIX}/share/hadoop/hdfs/hadoop-hdfs-2.5.1.jar \
 	-Xlint:deprecation \
 	*.java
