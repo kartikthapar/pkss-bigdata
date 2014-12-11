a635cd1023ff87fd941624db8971fee6840703c5
Simar Preet Singh <skoder91@gmail.com>
2014-12-10 23:32:57 -0500

    import for TextInput

--- MapRedKMeans/PKSSReducer.java
+++ MapRedKMeans/PKSSReducer.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import java.util.List;
 import java.util.ArrayList;
 
