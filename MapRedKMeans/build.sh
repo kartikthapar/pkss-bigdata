9dddb53d933920c61107e96f6433bba7846b7a7e
Paul O'Neil <redballoon36@gmail.com>
2014-12-11 04:58:09 +0000

    Get rid of MapRedKMeans since it moved into compression-schemes

--- MapRedKMeans/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-javac \
-	-classpath .:${HADOOP_PREFIX}/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.5.1.jar:${HADOOP_PREFIX}/share/hadoop/common/hadoop-common-2.5.1.jar:${HADOOP_PREFIX}/share/hadoop/mapreduce/lib/hadoop-annotations-2.5.1.jar \
-	-Xlint:deprecation \
-    -Xlint:unchecked \
-    edu/jhu/pkss/clustering/*.java
-
-jar cf PKSS.jar edu/jhu/pkss/clustering/*.class
