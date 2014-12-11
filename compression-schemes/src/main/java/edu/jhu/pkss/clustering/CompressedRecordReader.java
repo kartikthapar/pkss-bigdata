8cf964ae8800724bbe33abf9a5939c6f0fc03e00
Paul O'Neil <redballoon36@gmail.com>
2014-12-11 09:02:20 +0000

    Do arithmetic decompression

--- compression-schemes/src/main/java/edu/jhu/pkss/clustering/CompressedRecordReader.java
+++ compression-schemes/src/main/java/edu/jhu/pkss/clustering/CompressedRecordReader.java
@@ -1,60 +1,70 @@
 package edu.jhu.pkss.clustering;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+import nayuki.arithcode.AdaptiveArithmeticImpl;
 
 public class CompressedRecordReader extends org.apache.hadoop.mapreduce.RecordReader<Long, VectorizedObject>
 {
+    private long decompressed_size;
+    private long bytes_read;
+    private int objects_returned;
+    private int number_of_objects;
     private VectorizedObject curObj;
+    private byte[] byte_data;
 
     @Override
     public void close()
         throws IOException
     {
-        if (innerReader != null)
-            innerReader.close();
     }
 
     @Override
     public Long getCurrentKey()
         throws IOException, InterruptedException
     {
-        if (curObj != null)
-            return Long.parseLong(curObj.getKey());
+        if (!curObj)
+            return null;
         else
-            return -1L;
+            return Long.parseLong(curObj.getKey());
     }
 
     @Override
     public VectorizedObject getCurrentValue()
         throws IOException, InterruptedException
     {
-        return curObj;
+        return cur_obj;
     }
 
     @Override
     public float getProgress()
         throws IOException, InterruptedException
     {
-        return innerReader.getProgress();
+        return ((float) objects_returned) / number_of_objects;
     }
 
     @Override
     public boolean nextKeyValue()
         throws IOException, InterruptedException
     {
-        if (innerReader.nextKeyValue())
-        {
-            curObj = new VectorizedObject(innerReader.getCurrentValue().toString());
-            return true;
-        }
-        else
-        {
+        if (objects_returned == number_of_objects)
             return false;
-        }
+        long end_index;
+        for (end_index = bytes_read; end_index < byte_data.length && byte_data[end_index] != '\n'; ++end_index)
+        { }
+        String string_data = new String(byte_data, bytes_read, end_index - bytes_read);
+        curObj = new VectorizedObject(string_data);
+        return true;
     }
 
     @Override
@@ -62,15 +72,22 @@ public class CompressedRecordReader extends org.apache.hadoop.mapreduce.RecordRe
         throws IOException, InterruptedException
     {
         FileSplit fsplit = (FileSplit)split;
-        ByteBuffer buf = ByteBuffer.allocate(blockSize);
+        ByteBuffer buf = ByteBuffer.allocate((int)fsplit.getLength());
         
         Configuration conf = context.getConfiguration();
-        final Path file = split.getFile();
-        FileSystem fs = file.getFileSystem(conf):
+        final Path file = fsplit.getPath();
+        FileSystem fs = file.getFileSystem(conf);
         FSDataInputStream strm = fs.open(file);
-        strm.seek(split.getStart());
+        strm.seek(fsplit.getStart());
         strm.read(buf);
+        strm.close();
         
+        decompressed_size = buf.getLong();
+        objects_returned = 0;
+        number_of_objects = buf.getInt();
+        bytes_read = 0;
         
+        AdaptiveArithmeticImpl decompressor = new AdaptiveArithmeticImpl();
+        byte_data = decompressor.decompress(buf.slice().array());
     }
 }
