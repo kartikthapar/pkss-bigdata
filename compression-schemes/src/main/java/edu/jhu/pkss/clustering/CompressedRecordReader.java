e0dc285946f533376c17b8aa4f862c255f60d37b
Paul O'Neil <redballoon36@gmail.com>
2014-12-12 21:31:41 +0000

    Read the new compressed data header and only attempt to decompress the correct number of bytes
    Lots of code for debugging output

--- compression-schemes/src/main/java/edu/jhu/pkss/clustering/CompressedRecordReader.java
+++ compression-schemes/src/main/java/edu/jhu/pkss/clustering/CompressedRecordReader.java
@@ -1,12 +1,17 @@
 package edu.jhu.pkss.clustering;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.InputSplit;
@@ -22,11 +27,16 @@ public class CompressedRecordReader extends org.apache.hadoop.mapreduce.RecordRe
     private int number_of_objects;
     private VectorizedObject curObj;
     private byte[] byte_data;
+    private String string_data;
+
+    private Writer output;
 
     @Override
     public void close()
         throws IOException
     {
+        if (output != null)
+            output.close();
     }
 
     @Override
@@ -59,11 +69,16 @@ public class CompressedRecordReader extends org.apache.hadoop.mapreduce.RecordRe
     {
         if (objects_returned == number_of_objects)
             return false;
+        output.write("Starting scan for newline at index " + Integer.toString(bytes_read) + "\n");
         int end_index;
+        //for (end_index = bytes_read; end_index < string_data.length() && string_data.charAt(end_index) != '\n'; ++end_index)
         for (end_index = bytes_read; end_index < byte_data.length && byte_data[end_index] != '\n'; ++end_index)
         { }
-        String string_data = new String(byte_data, bytes_read, end_index - bytes_read);
-        curObj = new VectorizedObject(string_data);
+        
+        //String string_vector = string_data.substring(bytes_read, end_index - bytes_read);
+        String string_vector = new String(byte_data, bytes_read, end_index - bytes_read);
+        output.write("Found end of line at " + Integer.toString(end_index) + ", contents = " + string_vector + "\n");
+        curObj = new VectorizedObject(string_vector);
         bytes_read = end_index + 1;
         objects_returned += 1;
         return true;
@@ -85,12 +100,26 @@ public class CompressedRecordReader extends org.apache.hadoop.mapreduce.RecordRe
         strm.close();
         buf.rewind();
         
+        Path log_path = new Path(fsplit.getPath().toString() + "_log");
+        FSDataOutputStream outputFile;
+        if (fs.exists(log_path))
+            outputFile = fs.append(log_path);
+        else
+            outputFile = fs.create(log_path, (short)1);
+        output = new OutputStreamWriter(outputFile);
+
+        int compressed_size = buf.getInt();
         decompressed_size = buf.getLong();
         objects_returned = 0;
         number_of_objects = buf.getInt();
         bytes_read = 0;
         
+        output.write("Header - decompressed size: " + Long.toString(decompressed_size) + ", number_of_objects: " + Integer.toString(number_of_objects) + "\n");
+        
         AdaptiveArithmeticImpl decompressor = new AdaptiveArithmeticImpl();
-        byte_data = decompressor.decompress(buf.slice().array());
+        byte[] compressed_data = new byte[compressed_size];
+        buf.get(compressed_data);
+        byte_data = decompressor.decompress(compressed_data);
+        string_data = new String(byte_data, Charset.forName("UTF-8"));
     }
 }
